0d576051fb86713bd9370f81da729f400e00a030
Sergei Ampilov <sergei.ampilov@gmail.com>
2022-08-28 13:07:56 +0300

    feat: cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -15,6 +16,7 @@ const { processError, notFoundRequest } = require('./middlewares/error');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
